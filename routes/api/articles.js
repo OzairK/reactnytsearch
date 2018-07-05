1773b5eeb3b36746cae4549bd49e68c0339369cc
Ozair Khan <ozairsaddress@gmail.com>
2018-07-05 01:43:09 -0600

    working app

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -4,20 +4,12 @@ const articlesController = require("../../controllers/articlesController");
 // Matches with "/api/articles"
 router.route("/")
   .post(articlesController.create)
-  
+
+router.get("/yo", (req,res)=> res.send("hi"))
 
 router.route("/:id")
   .delete(articlesController.delete)
 router.route("/all")
   .get(articlesController.findAll)
-// Matches with "/api/books/:id"
-// router
-//   .route("/:id")
-//   .get(articlesController.findById)
-//   .put(articlesController.update)
-//   .delete(articlesController.remove);
-
-// router.route("/api/article")
-// .post(articlesController.create)
 
 module.exports = router;
--- server.js
+++ server.js
@@ -14,9 +14,10 @@ if (process.env.NODE_ENV === "production") {
 }
 // Add routes, both API and view
 app.use(routes);
+// app.get("/hi", function(req,res){res.send("hi")})
 
 
-app.get("/hello", function(req,res){res.send("stop playing around")});
+// app.get("/hello", function(req,res){res.send("stop playing around")});
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactarticles");
 mongoose.Promise = Promise;
